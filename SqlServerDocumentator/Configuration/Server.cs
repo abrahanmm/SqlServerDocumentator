a89e1f4f521ffa460f3e654465768cc2d58d250b
Abrahán Martínez Martín <abrahanmm@gmail.es>
2017-10-25 21:18:54 +0200

    Initial Commit

---
+++ SqlServerDocumentator/Configuration/Server.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlServerDocumentator.Configuration
+{
+    public class Server
+    {
+        public string Name { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
---
+++ SqlServerDocumentator/DocumentedDatabaseObjects/Table.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlServerDocumentator.DocumentedDatabaseObjects
+{
+    public class Table
+    {
+        public Table(string serverName, string tableName)
+        {
+            this.ServerName = serverName;
+            this.Name = tableName;
+        }
+
+        public string ServerName { get; }
+
+        public string Name { get; }
+    }
+}
