9446cae1950142d7da951010be8ec926734f9b80
Abrahán Martínez Martín <abrahanmm@gmail.es>
2017-10-26 23:09:35 +0200

    First test wit web

---
+++ WebSqlServerDocumentator/Controllers/DatabaseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SqlServerDocumentator;
+
+namespace WebSqlServerDocumentator.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        private IDocumentator Documentator;
+
+        public DatabaseController()
+        {
+            this.Documentator = SqlServerDocumentator.Configuration.ConfigurationProvider.CreateInstance();
+        }
+
+        [Route("/server/{serverName}")]
+        public IActionResult Index(string serverName)
+        {
+            return View(this.Documentator.GetDatabases(serverName));
+        }
+    }
+}
---
+++ WebSqlServerDocumentator/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebSqlServerDocumentator.Models;
+using SqlServerDocumentator;
+
+namespace WebSqlServerDocumentator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IDocumentator Documentator;
+
+        public HomeController()
+        {
+            this.Documentator = SqlServerDocumentator.Configuration.ConfigurationProvider.CreateInstance();
+        }
+
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View(this.Documentator.GetServers());
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
