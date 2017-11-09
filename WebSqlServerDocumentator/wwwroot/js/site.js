37c08ece00f93c472b37583f562d1dab75752b4b
Abrahán Martínez Martín <abrahanmm@gmail.es>
2017-11-09 20:47:18 +0100

    Servers with vue.js

--- WebSqlServerDocumentator/wwwroot/js/site.js
+++ WebSqlServerDocumentator/wwwroot/js/site.js
@@ -1,1 +1,16 @@
 ﻿// Write your JavaScript code.
+
+Vue.component('documented-server', {
+    props: {
+        name: { type: String, default: '' },
+        displayName: { type: String, default: '' },
+        description: { type: String, default: '' },
+        url: { type: String, default: '' }
+    },
+    template: '<div>{{displayName}} - {{name}} || {{description}} || <a :href="url">Explore</a></div>'
+});
+
+new Vue(
+    {
+        el: '.servers'
+    });
